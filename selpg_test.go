c1d6bb957ac9380f533aaf05ae6c3c1de50313ab
cyulei <1678240316@qq.com>
2018-10-31 11:01:54 -0400

    update test

--- selpg_test.go
+++ selpg_test.go
@@ -16,18 +16,23 @@ func TestUsage(t *testing.T) {
 }
 
 func Test_process_args(t *testing.T) {
-	type args struct {
-		args []string
-	}
+
 	tests := []struct {
 		name string
-		args args
+		args []string
 	}{
-	// TODO: Add test cases.
+		// TODO: Add test cases.
+		{
+			name: "Nil options",
+			args: []string{"selpg", "-s1", "-e1", "test.txt"},
+		}, {
+			name: "Nil",
+			args: []string{"selpg", "-s1", "-e2", "test.txt"},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			process_args(tt.args.args)
+			process_args(tt.args)
 		})
 	}
 }
